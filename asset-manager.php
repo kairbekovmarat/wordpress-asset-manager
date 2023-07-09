84d8a9dbedeb5009e1933b835f6ba246683432c5
Marat Kairbekov <kairbekovmarat@gmail.com>
2023-07-10 00:15:29 +0600

    Initial commit

---
+++ asset-manager.php
@@ -0,0 +1,12 @@
+<?php
+  /*
+    * Plugin Name: Asset Manager by Kanno Software
+    * Description: Easy asset management on the website
+    * Plugin URI: https://github.com/kanno-software/wordpress-asset-manager
+    * Version: 1.0
+    * Author: Kanno Software
+    * License: GPLv3
+    * License URI: https://www.gnu.org/licenses/quick-guide-gplv3.html
+    * Text Domain: asset-manager
+    * Domain Path: /languages
+    */
