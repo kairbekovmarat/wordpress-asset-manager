855e2f2a78117fb01e15ee675c99ccfd4f492754
Marat Kairbekov <kairbekovmarat@gmail.com>
2023-07-17 03:18:42 +0600

    Config and views files added

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+  define('AMKS_PLUGIN_DIR', plugin_dir_path(__FILE__));
+  define('AMKS_LANG_DOMAIN', 'kanno-am');
--- wordpress-asset-manager.php
+++ wordpress-asset-manager.php
@@ -11,9 +11,11 @@
     * Domain Path: /languages
     */
 
+    require_once(plugin_dir_path(__FILE__) . 'config.php');
+
     add_action('admin_menu', 'register_am_kanno_page');
     function register_am_kanno_page() {
-      $plugin_title = __('Asset Manager', 'kanno-am');
+      $plugin_title = __('Asset Manager', AMKS_LANG_DOMAIN);
       $parent_slug = 'kanno-am';
       $parent_callback = 'am_kanno_render_all_assets_page';
       $capability = 'manage_options';
@@ -30,7 +32,7 @@
       add_submenu_page(
         $parent_slug,
         $plugin_title,
-        __('All assets', 'kanno-am'),
+        __('All assets', AMKS_LANG_DOMAIN),
         $capability,
         $parent_slug,
         $parent_callback
@@ -39,8 +41,8 @@
       
       add_submenu_page(
         $parent_slug,
-        __('Dependencies - Asset Manager', 'kanno-am'),
-        __('Dependencies', 'kanno-am'),
+        __('Dependencies - Asset Manager', AMKS_LANG_DOMAIN),
+        __('Dependencies', AMKS_LANG_DOMAIN),
         $capability,
         'kanno-am-dependencies',
         'am_kanno_render_dependencies_page'
@@ -48,8 +50,8 @@
 
       add_submenu_page(
         $parent_slug,
-        __('Settings - Asset Manager', 'kanno-am'),
-        __('Settings', 'kanno-am'),
+        __('Settings - Asset Manager', AMKS_LANG_DOMAIN),
+        __('Settings', AMKS_LANG_DOMAIN),
         $capability,
         'kanno-am-settings',
         'am_kanno_render_settings_page'
@@ -57,8 +59,8 @@
 
       add_submenu_page(
         $parent_slug,
-        __('Documentation - Asset Manager', 'kanno-am'),
-        __('Documentation', 'kanno-am'),
+        __('Documentation - Asset Manager', AMKS_LANG_DOMAIN),
+        __('Documentation', AMKS_LANG_DOMAIN),
         $capability,
         'kanno-am-docs',
         'am_kanno_render_docs_page'
@@ -66,22 +68,22 @@
     }
 
     function am_kanno_render_all_assets_page() {
-      
+      require_once(AMKS_PLUGIN_DIR . 'views/all-assets.php');
     }
 
     function am_kanno_render_dependencies_page() {
-      
+      require_once(AMKS_PLUGIN_DIR . 'views/dependencies.php');
     }
 
     function am_kanno_render_settings_page() {
-      
+      require_once(AMKS_PLUGIN_DIR . 'views/settings.php');
     }
 
     function am_kanno_render_docs_page() {
-      
+      require_once(AMKS_PLUGIN_DIR . 'views/docs.php');
     }
 
     add_action('plugins_loaded', 'am_kanno_textdomain');
     function am_kanno_textdomain() {
-      load_plugin_textdomain('kanno-am', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/'); 
+      load_plugin_textdomain(AMKS_LANG_DOMAIN, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/'); 
     }
